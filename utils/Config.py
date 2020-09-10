9e1978329217492f6dcf9dcf11dcf5d001eb30b5
SmacUL <1164253125@qq.com>
2020-09-10 20:39:58 +0800

    up

--- utils/Config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-
-    def __init__(self, mod):
-        self.__medianPath = '../median'
-        self.__audiosPath = '../audios'
-        self.__videosPath = '../videos'
-        self.__resourcesPath = '../resources'
-        self.__mod = mod
